5d9002e402a83500e1a0d2562e49f27fa9e8039e
OTHMANEABEDKHATTABI <OTHMANEABEDKHATTABI@DESKTOP-T7MVIC8>
2025-05-04 12:39:05 +0200

    segunda comision, hemos borrado una linea y creado otra

--- src/pruebagit/Main.java
+++ src/pruebagit/Main.java
@@ -1,7 +1,7 @@
 package pruebagit;
 
-public class main {
+public class Main{
 	public static void main (String[]args) {
-		System.out.println("hola mundo");
+		System.out.println("hola otman");
 	} 
 }
